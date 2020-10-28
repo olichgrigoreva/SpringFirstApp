c03abd5aeb1a98f207586d12613a3a0ab5dcc58d
Olga <aboutrocky94@gmail.com>
2020-10-28 15:40:55 +0300

    11. Qualifier (конкретизация требуемого бина)

--- src/main/java/org/example/MusicPlayer.java
+++ src/main/java/org/example/MusicPlayer.java
@@ -1,20 +1,17 @@
 package org.example;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
 public class MusicPlayer {
-    private ClassicalMusic classicalMusic;
-
-    //IoC инверсия управления (объект извне)
     @Autowired
-    public MusicPlayer(ClassicalMusic music) {
-        this.classicalMusic = music; //полиморфизм; внедрение зависимости через конструктор
-    }
+    @Qualifier("rapMusic")
+    private Music music;
 
     public String playMusic() {
-        return "Playing: " + classicalMusic.getSong();
+        return "Playing: " + music.getSong();
     }
 
 }
--- src/main/java/org/example/RockMusic.java
+++ src/main/java/org/example/RockMusic.java
@@ -2,7 +2,7 @@ package org.example;
 
 import org.springframework.stereotype.Component;
 
-@Component("someRockMusic")
+@Component
 public class RockMusic implements Music{
 
     @Override
